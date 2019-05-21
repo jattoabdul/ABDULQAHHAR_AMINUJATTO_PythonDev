57f83c67f9aee27e5b454d3b57f21f87ba1aaa60
jattoabdul <jattoade@gmail.com>
2019-05-21 19:01:55 +0100

    update tests

--- compare_version_strings/tests/abdulqahhar_aminujatto_test.py
+++ compare_version_strings/tests/abdulqahhar_aminujatto_test.py
@@ -9,27 +9,27 @@ class TestClass:
                 "Should return a +ve number if first version is bigger than the second with same length [case 1]"
 
         def test_versions_same_length_first_bigger_case2(self):
-            assert compare_versions('1.4.  1.0', ' 1.4.0.6') > 0,\
+            assert compare_versions('1.4.1.0', '1.4.0.6') > 0,\
                 "Should return a +ve number if first version is bigger than the second with same length [case 2]"
 
         def test_versions_same_length_first_bigger_case3(self):
-            assert compare_versions(' 5', '1  ') > 0,\
+            assert compare_versions('6', '2') > 0,\
                 "Should return a +ve number if first version is bigger than the second with same length [case 3]"
 
         def test_versions_same_length_second_bigger_case1(self):
-            assert compare_versions(' 1 .2.0  ', ' 1.2 .1') < 0,\
+            assert compare_versions('1.2.0', ' 1.2.1') < 0,\
                 "Should return a +ve number if first version is smaller than the second with same length [case 1]"
 
         def test_versions_same_length_second_bigger_case2(self):
-            assert compare_versions('1.4.0.6  ', '  1.4.1.0') < 0,\
+            assert compare_versions('1.4.0.6', '1.4.1.0') < 0,\
                 "Should return a -ve number if first version is smaller than the second with same length [case 2]"
 
         def test_versions_same_length_second_bigger_case3(self):
-            assert compare_versions('1', '  5') < 0,\
+            assert compare_versions('2', '6') < 0,\
                 "Should return a -ve number if first version is smaller than the second with same length [case 3]"
 
         def test_versions_same_length_equals(self):
-            assert compare_versions(' 1.2.3  ', '1.2.3') == 0,\
+            assert compare_versions('1.2.3.4', '1.2.3.4') == 0,\
                 "Should return zero if are equal with same length"
 
         def test_versions_different_length_first_bigger_case1(self):
@@ -37,40 +37,40 @@ class TestClass:
                 "Should return a +ve number if first version is bigger than the second with different length [case 1]"
 
         def test_versions_different_length_first_bigger_case2(self):
-            assert compare_versions('1.4.  1.0.0.0.0.0.0', ' 1.4.0.6') > 0,\
+            assert compare_versions('1.4.1.0.0.0.0.0.0', '1.4.0.6') > 0,\
                 "Should return a +ve number if first version is bigger than the second with different length [case 2]"
 
         def test_versions_different_length_first_bigger_case3(self):
-            assert compare_versions(' 5', '1  .0.0.0. 0') > 0,\
+            assert compare_versions('6', '1.0.0.0.0') > 0,\
                 "Should return a +ve number if first version is bigger than the second with different length [case 3]"
 
         def test_versions_different_length_second_bigger_case1(self):
-            assert compare_versions(' 1 .2.0.2.2.3.5.7.1  ', ' 1.2 .1.0.0.1') < 0,\
+            assert compare_versions('1.2.0.2.2.3.5.7.1', '1.2.1.0.0.1') < 0,\
                 "Should return a -ve number if first version is smaller than the second with different length [case 1]"
 
         def test_versions_different_length_second_bigger_case2(self):
-            assert compare_versions('1.4.0.6.0.0  ', '  1.4.1.0.0.9.0.5') < 0,\
+            assert compare_versions('1.4.0.6.0.0', '1.4.1.0.0.9.0.5') < 0,\
                 "Should return a -ve number if first version is smaller than the second with different length [case 2]"
 
         def test_versions_different_length_second_bigger_case3(self):
-            assert compare_versions('1.0', '  5.9.8.5.2') < 0,\
+            assert compare_versions('1.0', '5.9.8.5.2') < 0,\
                 "Should return a -ve number if first version is smaller than the second with different length [case 3]"
 
         def test_versions_different_length_equals_case1(self):
-            assert compare_versions(' 1.2.3  .0.0.0.0.0.0', '1.2.3') == 0,\
+            assert compare_versions('1.2.3.0.0.0.0.0.0', '1.2.3') == 0,\
                 "Should return zero if are equal with different length [case 1]"
 
         def test_versions_different_length_equals_case2(self):
-            assert compare_versions(' 1.2.3.0.0.1.0', '1.2.3.0.0.1.0.0.0') == 0,\
+            assert compare_versions('1.2.3.0.0.1.0', '1.2.3.0.0.1.0.0.0') == 0,\
                 "Should return zero if are equal with different length [case 2]"
 
         def test_should_raise_error_if_not_string_parameter(self):
             with pytest.raises(AttributeError):
-                compare_versions('1', 2.5)
+                compare_versions('1', 3.6)
 
         def test_should_raise_error_if_unexpected_character(self):
             with pytest.raises(ValueError):
-                compare_versions('1.2.c', '1.b.1.a')
+                compare_versions('2.3.d.1', '2.b.4.a')
 
     class TestPrepareComparisonResult:
         def test_versions_comparison_result_string_case1(self):
